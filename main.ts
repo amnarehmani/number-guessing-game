143d5b616cd0b96a1075e94b7b3f82967e3f95a8
amnarehmani <jeonamnaaa@gmail.com>
2024-04-19 17:21:44 +0500

    number guessing game

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer"
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log("welcome to number guessing game");
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-6",
+    },
+]);
+if (answers.UserGuessedNumber === randomNumber) {
+    console.log("congratulations! you guessed right number");
+}
+else {
+    console.log("you guessed wrong number");
+}
